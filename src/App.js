b53c8e8aacc8e6f2f2538174598dd06755c69d9b
matiasea <matias.almada02@gmail.com>
2022-06-09 00:09:12 -0300

    "modifcamos nav con JSX"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+import Navigation from "./components/navigation";
+
+
 function App() {
   return (
     <div className="App">
-    
+      <Navigation/>    
       <h1>Mi Tienda Digital</h1>
       <h3>Almada Matias - React</h3>
     </div>
---
+++ src/components/navigation.js
@@ -0,0 +1,18 @@
+import "./navigation.css"
+
+const Navigation = () => {
+    return (
+        <nav>
+            <ul className="nav1">
+                <li>Inicio</li>
+                <li>Quienes somos</li>
+                <li>Nustros Productos</li>
+                <li>Postventa</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>
+    )
+};
+
+export default Navigation;
+ 
